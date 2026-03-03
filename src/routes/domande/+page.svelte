bc28829f93cb7cb887b7413e603589d4456c3edf
chamered <smaltauro@unibz.it>
2026-03-03 18:20:38 +0100

    Added a 'Fai una domanda' button in the '/domande' page

--- src/routes/domande/+page.svelte
+++ src/routes/domande/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Icon from "@iconify/svelte";
     import QuestionCard from '../../components/QuestionCard.svelte';
 
     let { data } = $props();
@@ -8,10 +9,20 @@
 </svelte:head>
 
 <div class="container py-3">
-    <h2 class="text-white">Tutte le Domande</h2>
-    <p class="text-light">Naviga tra tutte le domande fatte dalla nostra community.</p>
+    <div class="d-md-flex justify-content-between align-items-center mb-3">
+        <div>  
+            <h2 class="text-white">Tutte le Domande</h2>
+            <p class="text-light m-0">Naviga tra tutte le domande fatte dalla nostra community.</p>
+        </div>
+        <div>
+            <a class="btn btn-custom d-flex justify-content-center align-items-center gap-1 my-3" href="/">
+                <Icon icon="icon-park-outline:write" width="18" height="18" />
+                Fai una Domanda
+            </a>
+        </div>
+    </div>
     
-    <div class="row mt-4 g-3 h-100">
+    <div class="row g-3 h-100">
         {#each data.domande as item}
             <div class="col-12">
                 <QuestionCard item={item}/>
