087a831bd2b010226e50f1bb9b889141a1e84dd3
chamered <smaltauro@unibz.it>
2026-03-06 10:32:43 +0100

    refactor: update Navbar to use an imported logo asset.

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,11 +1,12 @@
 <script>
     import UserMenu from "./UserMenu.svelte";
+    import logo from "$lib/assets/trinitalk-word-logo.png";
 </script>
 
 <nav class="navbar navbar-expand-lg bg-custom border-bottom border-custom-2 border-2">
     <div class="container-fluid">
         <a href="/" class="navbar-brand d-flex align-items-center">
-            <img src="src/lib/assets/trinitalk-word-logo.png" alt="Trinitalk" width="100">
+            <img src={logo} alt="Trinitalk" width="100">
         </a>
         <ul class="navbar-nav">
             <li class="nav-item">
